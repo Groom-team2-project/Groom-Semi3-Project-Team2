f02e370729b9a47dbd02adba37314f6dc8f8fa08
Hi0Park <pgi668.YWP@gmail.com>
2026-08-13 20:21:30 +0900

    feat: Global Exception Handler 추가

---
+++ src/main/java/com/groom/moigo/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,45 @@
+package com.groom.moigo.global.error;
+
+import com.groom.moigo.global.response.CommonResponse;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<CommonResponse<Void>> handleBusinessException(
+        BusinessException exception
+    ) {
+        ErrorCode errorCode = exception.getErrorCode();
+
+        return ResponseEntity
+                .status(errorCode.getStatus())
+                .body(CommonResponse.error(
+                        errorCode.name(),
+                        errorCode.getMessage()
+                ));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CommonResponse<Void>> handleValidationException(
+            MethodArgumentNotValidException exception
+    ) {
+        String message = exception.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .findFirst()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .orElse(ErrorCode.INVALID_INPUT_VALUE.getMessage());
+
+        return ResponseEntity
+                .badRequest()
+                .body(CommonResponse.error(
+                        ErrorCode.INVALID_INPUT_VALUE.name(),
+                        message
+                ));
+    }
+}
