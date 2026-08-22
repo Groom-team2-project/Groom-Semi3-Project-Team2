6541c944c8504470f4536a553c665c79d52e1237
mmy428 <mmy428@naver.com>
2026-08-22 17:22:58 +0900

    fix: migration V9 수정

--- src/main/resources/db/migration/V9__add_schedule_column.sql
+++ src/main/resources/db/migration/V9__add_schedule_column.sql
@@ -2,7 +2,14 @@ ALTER TABLE schedules
     ADD COLUMN end_at DATETIME(6) NULL AFTER start_at;
 
 UPDATE schedules
-SET end_at = TIMESTAMP(DATE(start_at), end_time)
+SET end_at = CAST(
+        CONCAT(
+                CAST(start_at AS DATE),
+                ' ',
+                end_time
+        )
+    AS DATETIME
+             )
 WHERE end_time IS NOT NULL;
 
 ALTER TABLE schedules
