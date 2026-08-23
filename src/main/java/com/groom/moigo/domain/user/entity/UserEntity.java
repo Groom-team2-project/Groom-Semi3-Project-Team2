4b4a7e1037b39108c2e3e29e74f9e74604c5cabd
Hi0Park <pgi668.YWP@gmail.com>
2026-08-23 14:56:56 +0900

    fix: 사용자 닉네임 nullable 설정 반영

--- src/main/java/com/groom/moigo/domain/user/entity/UserEntity.java
+++ src/main/java/com/groom/moigo/domain/user/entity/UserEntity.java
@@ -34,7 +34,7 @@ public class UserEntity {
     @Column(name = "email", length = 255, unique = true, nullable = false)
     private String email;
 
-    @Column(name = "nickname", nullable = false, length = 50)
+    @Column(name = "nickname", nullable = true, length = 50)
     private String nickname;
 
     @Column(name = "profile_image", length = 500)
