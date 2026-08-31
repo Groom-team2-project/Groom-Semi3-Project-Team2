7229ea1f368bd622c7d0bf3a2d683c64a6af1183
Hi0Park <pgi668.YWP@gmail.com>
2026-08-31 22:18:00 +0900

    fix: Place API 기본 경로 통일

--- src/main/java/com/groom/moigo/domain/place/controller/PlaceController.java
+++ src/main/java/com/groom/moigo/domain/place/controller/PlaceController.java
@@ -21,7 +21,7 @@ import java.math.BigDecimal;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/place2")
+@RequestMapping("/api/v1/place")
 public class PlaceController {
     private final PlaceService placeService;
 
